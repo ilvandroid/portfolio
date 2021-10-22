68fb6bf1b79fb03a3c6fcd97be73d2a7067a8e7c
SUNNY KUMAR GUPTA <50216234+ilvandroid@users.noreply.github.com>
2021-10-22 17:17:56 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        //setting-name: setting-value
+        arrows: false,
+        dots: true,
+        appendDots: '.slider-dots',
+        dotsClass: 'dots'
+        //dotsClass:''
+    });
+});
+
+
+/*$(document).ready(function(){
+$('.slider').slick({
+
+});
+
+}); */
